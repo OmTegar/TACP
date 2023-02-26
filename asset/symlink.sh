4456335cfe3c9362186a5198f1b9463d04fcf63b
TEGAR_71105 <tegardito02@gmail.com>
2023-02-27 01:35:03 +0700

    update

--- asset/symlink.sh
+++ asset/symlink.sh
@@ -5,5 +5,5 @@ source ./upgrade.sh
 if [ -L /asset/WebStatis/tegar/upgrade.sh ]; then
     echo "Symbolic link already exists"
 else
-    progress_bar ln -s asset/upgrade.sh asset/WebStatis/tegar/
+    ln -s asset/upgrade.sh asset/WebStatis/tegar/
 fi
