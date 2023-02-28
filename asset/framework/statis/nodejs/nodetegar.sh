f50c6439afbb4d5ead1186139c54bdeabda42ec3
TEGAR_71105 <tegardito02@gmail.com>
2023-02-28 21:49:45 +0700

    update

--- asset/framework/statis/nodejs/nodetegar.sh
+++ asset/framework/statis/nodejs/nodetegar.sh
@@ -41,65 +41,65 @@ cd node-website-static1/
 
 # buat index baru dengan EOF include dengan port custom
 cat << EOF > index.js
-console.log('var http = require("http");');
-console.log('var fs = require("fs");');
-console.log('var path = require("path");');
-console.log('var port = $port;');
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var port = $port;
 
-console.log('http');
-console.log('  .createServer(function (request, response) {');
-console.log('    console.log("request ", request.url);');
+http
+  .createServer(function (request, response) {
+    console.log("request ", request.url);
 
-console.log('    var filePath = "." + request.url;');
-console.log('    if (filePath == "./") {');
-console.log('      filePath = "./app/index.html";');
-console.log('    }');
+    var filePath = "." + request.url;
+    if (filePath == "./") {
+      filePath = "./app/index.html";
+    }
 
-console.log('    var extname = String(path.extname(filePath)).toLowerCase();');
-console.log('    var mimeTypes = {');
-console.log('      ".html": "text/html",');
-console.log('      ".js": "text/javascript",');
-console.log('      ".css": "text/css",');
-console.log('      ".json": "application/json",');
-console.log('      ".png": "image/png",');
-console.log('      ".jpg": "image/jpg",');
-console.log('      ".gif": "image/gif",');
-console.log('      ".svg": "image/svg+xml",');
-console.log('      ".wav": "audio/wav",');
-console.log('      ".mp4": "video/mp4",');
-console.log('      ".woff": "application/font-woff",');
-console.log('      ".ttf": "application/font-ttf",');
-console.log('      ".eot": "application/vnd.ms-fontobject",');
-console.log('      ".otf": "application/font-otf",');
-console.log('      ".wasm": "application/wasm",');
-console.log('    };');
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+      ".html": "text/html",
+      ".js": "text/javascript",
+      ".css": "text/css",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpg",
+      ".gif": "image/gif",
+      ".svg": "image/svg+xml",
+      ".wav": "audio/wav",
+      ".mp4": "video/mp4",
+      ".woff": "application/font-woff",
+      ".ttf": "application/font-ttf",
+      ".eot": "application/vnd.ms-fontobject",
+      ".otf": "application/font-otf",
+      ".wasm": "application/wasm",
+    };
 
-console.log('    var contentType = mimeTypes[extname] || "application/octet-stream";');
+    var contentType = mimeTypes[extname] || "application/octet-stream";
 
-console.log('    fs.readFile(filePath, function (error, content) {');
-console.log('      if (error) {');
-console.log('        if (error.code == "ENOENT") {');
-console.log('          fs.readFile("./404.html", function (error, content) {');
-console.log('            response.writeHead(404, { "Content-Type": "text/html" });');
-console.log('            response.end(content, "utf-8");');
-console.log('          });');
-console.log('        } else {');
-console.log('          response.writeHead(500);');
-console.log('          response.end(');
-console.log('            "Sorry, check with the site admin for error: " +');
-console.log('              error.code +');
-console.log('              " ..\\n"');
-console.log('          );');
-console.log('        }');
-console.log('      } else {');
-console.log('        response.writeHead(200, { "Content-Type": contentType });');
-console.log('        response.end(content, "utf-8");');
-console.log('      }');
-console.log('    });');
-console.log('  })');
-console.log('  .listen(port);');
+    fs.readFile(filePath, function (error, content) {
+      if (error) {
+        if (error.code == "ENOENT") {
+          fs.readFile("./404.html", function (error, content) {
+            response.writeHead(404, { "Content-Type": "text/html" });
+            response.end(content, "utf-8");
+          });
+        } else {
+          response.writeHead(500);
+          response.end(
+            "Sorry, check with the site admin for error: " +
+              error.code +
+              " ..\n"
+          );
+        }
+      } else {
+        response.writeHead(200, { "Content-Type": contentType });
+        response.end(content, "utf-8");
+      }
+    });
+  })
+  .listen(port);
 
-console.log('console.log("Server running at http://127.0.0.1:" + port);');
+console.log("Server running at http://127.0.0.1:" + port);
 EOF
 
 
