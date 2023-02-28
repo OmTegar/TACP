063de94f3d05b2adb562b256b769fd120d2ff460
TEGAR_71105 <tegardito02@gmail.com>
2023-02-28 23:14:45 +0700

    update

--- asset/WebStatis/sektema/sektema.sh
+++ asset/WebStatis/sektema/sektema.sh
@@ -40,11 +40,44 @@ chmod 777 -R /var/www/html/company-profile-sektema/
 
 # Replace the default Apache2 configuration with the custom configuration
 cd /etc/apache2/sites-available/
-rm -r /etc/apache2/sites-available/000-default.conf
-cp /var/www/html/company-profile-sektema/shell/000-default.conf .
-rm ../sites-enabled/000-default.conf
-cp 000-default.conf ../sites-enabled/
-cd ../../../
+cat << EOF > 000-default.conf
+<VirtualHost *:80>
+        # The ServerName directive sets the request scheme, hostname and port that
+        # the server uses to identify itself. This is used when creating
+        # redirection URLs. In the context of virtual hosts, the ServerName
+        # specifies what hostname must appear in the request's Host: header to
+        # match this virtual host. For the default virtual host (this file) this
+        # value is not decisive as it is used as a last resort host regardless.
+        # However, you must set it for any further virtual host explicitly.
+        #ServerName www.example.com
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html/company-profile-sektema/
+
+        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for particular
+        # modules, e.g.
+        #LogLevel info ssl:warn
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        # For most configuration files from conf-available/, which are
+        # enabled or disabled at a global level, it is possible to
+        # include a line for only one particular virtual host. For example the
+        # following line enables the CGI configuration for this host only
+        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+EOF
+ln -s /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-enabled/
+
+# rm -r /etc/apache2/sites-available/000-default.conf
+# cp /var/www/html/company-profile-sektema/shell/000-default.conf .
+# rm ../sites-enabled/000-default.conf
+# cp 000-default.conf ../sites-enabled/
+# cd ../../../
 
 # Restart Apache2 service
 systemctl restart apache2
