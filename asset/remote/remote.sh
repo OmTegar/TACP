1333975ccb999dcc7da9ea94a44421d54ce7b463
Tegar Dito Priandika <tegardito02@gmail.com>
2023-03-03 06:15:06 +0000

    update

--- asset/remote/remote.sh
+++ asset/remote/remote.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
+source ./asset/upgrade.sh
 
 clear
-banner="#####################################################################
-                                                                
-  8888888 8888888888   .8.           ,o888888o.    8 888888888o   
-        8 8888        .888.         8888      88.  8 8888     88. 
-        8 8888       :88888.     ,8 8888        8. 8 8888      88 
-        8 8888      .  88888.    88 8888           8 8888      88 
-        8 8888     .8.  88888.   88 8888           8 8888.   ,88  
-        8 8888    .8 8.  88888.  88 8888           8 888888888P'  
-        8 8888   .8'  8.  88888. 88 8888           8 8888         
-        8 8888  .8'    8.  88888. 8 8888       .8  8 8888         
-        8 8888 .888888888.  88888.  8888     ,88'  8 8888         
-        8 8888.8'        8.  88888.   8888888P'    8 8888         
-                                                                  
-#####################################################################"
-echo "$banner"
+echo -e "${banner}${RESET}"
 sleep 2
 
-rsa="
-INPUT RSA PRIVATE KEY ANDA / .PEM                                                                                                                                                                                                                                                                  
-"
-echo "$rsa"
+message "INPUT RSA PRIVATE KEY ANDA / .PEM"
 sleep 4
 
 #menyimpan RSA pada directory (/)
@@ -29,7 +13,8 @@ cd / && nano labsuser.pem
 chmod 400 labsuser.pem
 
 #remote OS
-echo "Masukkan IP Server Yang Akan Anda Remote :"
+message "Masukkan IP Server Yang Akan Anda Remote :"
+echo "Your Answer : "
 read ip
 
 echo "Pilih OS Server Yang akan Di Remote"
@@ -40,26 +25,11 @@ select pil in "${pil[@]}"; do
         ssh -i labsuser.pem ubuntu@$ip <<EOF
 sudo su
 cd /
-git clone https://github.com/OmTegar/TACP.git
+clone_repo "https://github.com/OmTegar/TACP.git"
 cd /Fiture-OmTegar
 chmod +x index.sh
-banner="#####################################################################
-                                                                
-  8888888 8888888888   .8.           ,o888888o.    8 888888888o   
-        8 8888        .888.         8888      88.  8 8888     88. 
-        8 8888       :88888.     ,8 8888        8. 8 8888      88 
-        8 8888      .  88888.    88 8888           8 8888      88 
-        8 8888     .8.  88888.   88 8888           8 8888.   ,88  
-        8 8888    .8 8.  88888.  88 8888           8 888888888P'  
-        8 8888   .8'  8.  88888. 88 8888           8 8888         
-        8 8888  .8'    8.  88888. 8 8888       .8  8 8888         
-        8 8888 .888888888.  88888.  8888     ,88'  8 8888         
-        8 8888.8'        8.  88888.   8888888P'    8 8888         
-                                                                  
-#####################################################################"
-echo "$banner"
-echo ""
-echo "Anda Sudah Berada di Remote Server Yang Anda Inginkan"
+echo -e "${banner}${RESET}"
+message "Anda Sudah Berada di Remote Server Yang Anda Inginkan"
 EOF
         ssh -i labsuser.pem ubuntu@$ip
         ;;
@@ -67,28 +37,13 @@ EOF
         ssh -i labsuser.pem admin@$ip <<EOF
 sudo su
 apt-get update 
-apt install git -y
+apt install git -y & progress_bar $! 
 cd /
-git clone https://github.com/OmTegar/TACP.git
+clone_repo  "https://github.com/OmTegar/TACP.git"
 cd /Fiture-OmTegar
 chmod +x index.sh
-banner="#####################################################################
-                                                                
-  8888888 8888888888   .8.           ,o888888o.    8 888888888o   
-        8 8888        .888.         8888      88.  8 8888     88. 
-        8 8888       :88888.     ,8 8888        8. 8 8888      88 
-        8 8888      .  88888.    88 8888           8 8888      88 
-        8 8888     .8.  88888.   88 8888           8 8888.   ,88  
-        8 8888    .8 8.  88888.  88 8888           8 888888888P'  
-        8 8888   .8'  8.  88888. 88 8888           8 8888         
-        8 8888  .8'    8.  88888. 8 8888       .8  8 8888         
-        8 8888 .888888888.  88888.  8888     ,88'  8 8888         
-        8 8888.8'        8.  88888.   8888888P'    8 8888         
-                                                                  
-#####################################################################"
-echo "$banner"
-echo ""
-echo "Anda Sudah Berada di Remote Server Yang Anda Inginkan"
+echo -e "${banner}${RESET}"
+message "Anda Sudah Berada di Remote Server Yang Anda Inginkan"
 EOF
         ssh -i labsuser.pem admin@$ip
         break
