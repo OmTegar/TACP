af64b06e2ef238449fa1239907d4123383e07a17
TEGAR_71105 <tegardito02@gmail.com>
2023-06-06 09:08:29 +0700

    update

--- asset/DatabaseServer/database.sh
+++ asset/DatabaseServer/database.sh
@@ -21,11 +21,11 @@ y
 y
 EOF
 
-message "Masukkan Username Mysql yang anda inginkan :" 
+message "Masukkan Username MySQL yang anda inginkan :" 
 echo "Your Answer : "
 read Username
 
-message "Masukkan Password Mysql yang anda inginkan :" 
+message "Masukkan Password MySQL yang anda inginkan :" 
 echo "Your Answer : "
 read Password
 
@@ -39,16 +39,8 @@ GRANT INSERT ON *.* TO '$Username'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-# message "Secara Deffauld Anda Dapat login Menggunakan username "root" dan Password "root""
-# message "Anda Dapat Tetap Menggunakan User Yang Sudah Anda Gunakan Dengan Username "$Username" Dan Password  "$Password""
-
-
 cd /home & mkdir database
 chmod 777 /home/database
-
-# Baru
-# GRANT ALL PRIVILEGES ON *.* TO 'root'@'3.90.50.239' IDENTIFIED BY 'root' WITH GRANT OPTION;
-# FLUSH PRIVILEGES;
 
 # Allow remote access to MariaDB Server
 sudo sed -i '/bind-address/ s/^#*/#/' /etc/mysql/mariadb.conf.d/50-server.cnf
@@ -64,56 +56,20 @@ sudo systemctl restart apache2
 
 echo "Instalasi dan konfigurasi selesai. Anda dapat mengakses phpMyAdmin dari instance yang berbeda menggunakan pengguna 'tegar' dan kata sandi 'rahasia'."
 
-cd /etc/apache2/sites-available/
-cat <<EOF >000-default.conf
-<VirtualHost *:80>
-        # The ServerName directive sets the request scheme, hostname and port that
-        # the server uses to identify itself. This is used when creating
-        # redirection URLs. In the context of virtual hosts, the ServerName
-        # specifies what hostname must appear in the request's Host: header to
-        # match this virtual host. For the default virtual host (this file) this
-        # value is not decisive as it is used as a last resort host regardless.
-        # However, you must set it for any further virtual host explicitly.
-        #ServerName www.example.com
-
-        ServerAdmin webmaster@localhost
-        DocumentRoot /var/www/html/phpmyadmin/
-
-        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
-        # error, crit, alert, emerg.
-        # It is also possible to configure the loglevel for particular
-        # modules, e.g.
-        #LogLevel info ssl:warn
-
-        ErrorLog ${APACHE_LOG_DIR}/error.log
-        CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-        # For most configuration files from conf-available/, which are
-        # enabled or disabled at a global level, it is possible to
-        # include a line for only one particular virtual host. For example the
-        # following line enables the CGI configuration for this host only
-        # after it has been globally disabled with "a2disconf".
-        #Include conf-available/serve-cgi-bin.conf
-</VirtualHost>
-EOF
-
-# Restart Apache2 service
-sudo systemctl restart apache2
-
 clear
 echo -e "${banner}${RESET}"
 sleep 2
 echo " "
-message "Berikut Data User Mysql Server Anda:" > /home/database/user.txt
+message "Berikut Data User MySQL Server Anda:" > /home/database/user.txt
 echo "#############################" >> /home/database/user.txt
-echo "Username Default  = root "         >> /home/database/user.txt
-echo "Password Default  = root"    >> /home/database/user.txt
-echo " "
-echo "Username          = $Username"          >> /home/database/user.txt
-echo "Password          = $Password"      >> /home/database/user.txt
+echo "| Username Default  | root   |" >> /home/database/user.txt
+echo "| Password Default  | root   |" >> /home/database/user.txt
+echo "| Username          | $Username   |" >> /home/database/user.txt
+echo "| Password          | $Password   |" >> /home/database/user.txt
 echo "#############################" >> /home/database/user.txt
 cat /home/database/user.txt
 echo " "
+
 
 
 # nitip
