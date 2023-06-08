b7bc8d8a0c27f9e4294168e9a1b9d76a59480fd0
TEGAR_71105 <tegardito02@gmail.com>
2023-06-08 21:30:19 +0700

    bug fix rsync

--- asset/RsyncHelper/rsync.sh
+++ asset/RsyncHelper/rsync.sh
@@ -56,7 +56,6 @@ perform_local_rsync() {
       ;;
     *)
       error_message "Pilihan tidak valid. Menggunakan opsi default: -avz"
-      echo "Your Answer : "
       options="avz"
       ;;
   esac
@@ -66,6 +65,13 @@ perform_local_rsync() {
 
   # Menjalankan perintah rsync
   $rsync_command
+
+  # Memeriksa status keluaran rsync
+  if [ $? -eq 0 ]; then
+    success_message "Rsync berhasil dilakukan."
+  else
+    error_message "Rsync gagal dilakukan."
+  fi
 }
 
 # Fungsi untuk melakukan rsync ke server remote
@@ -117,120 +123,55 @@ perform_remote_rsync() {
       ;;
     *)
       error_message "Pilihan tidak valid. Menggunakan opsi default: -avz"
-      echo "Your Answer : "
       options="avz"
       ;;
   esac
 
-  # Mendapatkan server sumber dan tujuan dari pengguna
-  message "Masukkan server sumber dan tujuan (username@server):"
+  # Mendapatkan informasi server remote dari pengguna
+  message "Masukkan informasi server remote:"
   echo "Your Answer : "
-  read -a servers
+  read -p "Username:" username
+  read -p "Hostname:" hostname
+  read -p "Port (default 22):" port
 
-  if [ ${#servers[@]} -lt 2 ]; then
-    error_message "Server sumber dan tujuan harus diisi."
-    return
-  fi
-
-  source_server="${servers[0]}"
-  destination_server="${servers[1]}"
-
-  # Mendapatkan metode otentikasi untuk server tujuan
-  message "Pilih metode otentikasi untuk server tujuan:"
-  echo "+-----+-------------------------+----------------------------------------------+"
-  echo "| No  |         Metode          |                  Keterangan                 |"
-  echo "+-----+-------------------------+----------------------------------------------+"
-  echo "|  1  | Password                | Menggunakan password untuk otentikasi       |"
-  echo "|  2  | File .pem               | Menggunakan file .pem untuk otentikasi      |"
-  echo "+-----+-------------------------+----------------------------------------------+"
-  echo "Your Answer : "
-  read -p "Masukkan nomor pilihan (1 atau 2):" auth_method
-
-  # Mendapatkan informasi otentikasi berdasarkan metode yang dipilih
-  case $auth_method in
-    1)
-      message "Masukkan password untuk otentikasi server tujuan:"
-      echo "Your Answer : "
-      read -s destination_password
-      rsync_command="rsync -$options --progress -e 'sshpass -p $destination_password ssh' $source_path $destination_server:$destination_path"
-      ;;
-    2)
-      message "Masukkan path ke file .pem:"
-      echo "Your Answer : "
-      read pem_path
-      rsync_command="rsync -$options --progress -e 'ssh -i $pem_path' $source_path $destination_server:$destination_path"
-      ;;
-    *)
-      error_message "Pilihan tidak valid. Menggunakan metode default: Password"
-      message "Masukkan password untuk otentikasi server tujuan:"
-      echo "Your Answer : "
-      read -s destination_password
-      rsync_command="rsync -$options --progress -e 'sshpass -p $destination_password ssh' $source_path $destination_server:$destination_path"
-      ;;
-  esac
+  # Perintah rsync
+  rsync_command="rsync -$options --progress -e 'ssh -p $port' $source_path $username@$hostname:$destination_path"
 
   # Menjalankan perintah rsync
   $rsync_command
-}
 
-# Fungsi untuk menambahkan cronjob
-add_cronjob() {
-  # Mendapatkan path dari directory HasilRsync.sh
-  rsync_directory="/home/Cronjob-TACP"
-  rsync_script_path="$rsync_directory/HasilRsync_$(date +'%Y%m%d%H%M%S').sh"
-
-  # Memeriksa apakah direktori rsync-TACP sudah ada atau belum
-  if [ ! -d "$rsync_directory" ]; then
-    # Membuat direktori baru jika belum ada
-    mkdir -p "$rsync_directory"
-  fi
-
-  # Menanyakan pengguna apakah ingin menambahkan cronjob
-  message "Apakah Anda ingin menambahkan cronjob untuk script rsync? (y/n)"
-  echo "Your Answer : "
-  read cronjob_option
-
-  if [ "$cronjob_option" = "y" ]; then
-    # Menanyakan pengguna waktu (hitungan menit) untuk menjalankan cronjob
-    message "Masukkan waktu cronjob (hitungan menit):"
-    echo "Your Answer : "
-    read cron_minutes
-
-    # Validasi apakah input waktu cron adalah angka
-    if [[ "$cron_minutes" =~ ^[0-9]+$ ]]; then
-      # Menambahkan cronjob untuk menjalankan script rsync
-      (crontab -l ; echo "$cron_minutes * * * * /bin/bash $rsync_script_path") | crontab -
-      message "Cronjob berhasil ditambahkan."
-    else
-      error_message "Waktu cron tidak valid. Cronjob tidak ditambahkan."
-    fi
+  # Memeriksa status keluaran rsync
+  if [ $? -eq 0 ]; then
+    success_message "Rsync berhasil dilakukan."
   else
-    message "Cronjob tidak ditambahkan."
+    error_message "Rsync gagal dilakukan."
   fi
 }
 
+# Menampilkan menu utama
+while true; do
+  echo "+-----------------------------------------+"
+  echo "|                 Menu                    |"
+  echo "+-----------------------------------------+"
+  echo "|  1. Rsync Lokal                         |"
+  echo "|  2. Rsync ke Server Remote              |"
+  echo "|  3. Keluar                              |"
+  echo "+-----------------------------------------+"
+  echo "Your Answer : "
+  read -p "Pilih menu (1-3):" choice
 
-# Mendapatkan mode rsync dari pengguna
-message "Pilih mode rsync:"
-echo "+-----+-----------------------+------------------------------------+"
-echo "| No  |         Mode          |            Keterangan              |"
-echo "+-----+-----------------------+------------------------------------+"
-echo "|  1  | Rsync lokal           | Melakukan rsync pada localhost     |"
-echo "|  2  | Rsync ke server remote| Melakukan rsync ke server remote   |"
-echo "+-----+-----------------------+------------------------------------+"
-read -p "Masukkan pilihan Anda (1 atau 2):" rsync_mode
-
-case $rsync_mode in
-  1)
-    perform_local_rsync
-    ;;
-  2)
-    perform_remote_rsync
-    ;;
-  *)
-    error_message "Pilihan tidak valid."
-    ;;
-esac
-
-# Menanyakan pengguna untuk menambahkan cronjob
-add_cronjob
+  case $choice in
+    1)
+      perform_local_rsync
+      ;;
+    2)
+      perform_remote_rsync
+      ;;
+    3)
+      exit 0
+      ;;
+    *)
+      error_message "Pilihan tidak valid."
+      ;;
+  esac
+done
