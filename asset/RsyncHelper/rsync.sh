deb54db44710bea71471f7d2c884388addd67c73
TEGAR_71105 <tegardito02@gmail.com>
2023-06-09 11:07:28 +0700

    update rsync  pem fiture

--- asset/RsyncHelper/rsync.sh
+++ asset/RsyncHelper/rsync.sh
@@ -116,8 +116,8 @@ perform_local_rsync() {
   fi
 }
 
-# Fungsi untuk melakukan rsync ke server remote
-perform_remote_rsync() {
+# Fungsi untuk melakukan rsync ke server remote menggunakan password
+perform_remote_rsync_with_password() {
   # Mendapatkan path sumber dari pengguna
   message "Masukkan path sumber:"
   read -p "Your Answer : " source_path
@@ -155,11 +155,12 @@ perform_remote_rsync() {
       options+="P"
       ;;
     5)
-      warning_message "Masukkan opsi kustom (pisahkan dengan spasi):"
+      warning_message "Masukkan opsi kustom (tanpa tanda minus [-] atau spasi):"
       read -r -a custom_options_array
       options+="${custom_options_array[*]}"
       ;;
     *)
+      # Memisahkan opsi menjadi karakter individu
       options=$(echo "$rsync_options" | sed 's/./& /g')
       ;;
   esac
@@ -179,6 +180,7 @@ perform_remote_rsync() {
 
   # Perintah rsync
   rsync_command="sshpass -p '$password' rsync -$options --progress -e 'ssh -p $port' $source_path $username@$hostname:$destination_path"
+
   # Menjalankan perintah rsync
   eval "$rsync_command"
 
@@ -223,36 +225,201 @@ perform_remote_rsync() {
   else
     error_message "Rsync gagal dilakukan."
   fi
-  #ini adalah perintah yang di butuhkan server target 
-  #sudo chown -R ec2-user:ec2-user /home/ec2-user/app-log/
-  #sudo chmod -R 755 /home/ec2-user/app-log/
+}
+
+# Fungsi untuk melakukan rsync ke server remote menggunakan private key .pem
+perform_remote_rsync_with_pem() {
+  # Mendapatkan path sumber dari pengguna
+  message "Masukkan path sumber:"
+  read -p "Your Answer : " source_path
+
+  # Mendapatkan path tujuan dari pengguna
+  message "Masukkan path tujuan:"
+  read -p "Your Answer : " destination_path
+
+  # Mendapatkan opsi rsync dari pengguna
+  message "Pilih opsi rsync:"
+  echo "+-----+---------------------+---------------------------------------------------------+"
+  echo "| No  |        Opsi         |                      Keterangan                         |"
+  echo "+-----+---------------------+---------------------------------------------------------+"
+  echo "|  1  | -a                  | Mode arsip (rekursif, mempertahankan izin, dll.)        |"
+  echo "|  2  | -v                  | Keluaran verbose                                        |"
+  echo "|  3  | -z                  | Kompresi data file selama transfer                      |"
+  echo "|  4  | -P                  | Setara dengan -v --partial --progress                   |"
+  echo "|  5  | Opsi kustom         | Opsi kustom                                             |"
+  echo "+-----+---------------------+---------------------------------------------------------+"
+  read -p "Masukkan nomor pilihan (nomor yang dipisahkan koma atau '5' untuk opsi kustom):" rsync_options
+
+  # Mengatur opsi rsync berdasarkan pilihan pengguna
+  options=""
+  case $rsync_options in
+    1)
+      options+="a"
+      ;;
+    2)
+      options+="v"
+      ;;
+    3)
+      options+="z"
+      ;;
+    4)
+      options+="P"
+      ;;
+    5)
+      warning_message "Masukkan opsi kustom (tanpa tanda minus [-] atau spasi):"
+      read -r -a custom_options_array
+      options+="${custom_options_array[*]}"
+      ;;
+    *)
+      # Memisahkan opsi menjadi karakter individu
+      options=$(echo "$rsync_options" | sed 's/./& /g')
+      ;;
+  esac
+
+  # Memisahkan opsi kustom menjadi opsi individual
+  custom_options_array=($options)
+  for opt in "${custom_options_array[@]}"; do
+    options+="$opt"
+  done
+
+  # Mendapatkan informasi server remote dari pengguna
+  message "Masukkan informasi server remote:"
+  read -p "Username:" username
+  read -p "Hostname:" hostname
+  read -p "Port (default 22):" port
+
+  message "Apakah Anda ingin menggunakan RSA Private Key yang sudah ada? (y/n)"
+  read -p "Your Answer : " use_existing_key
+
+  if [ "$use_existing_key" == "y" ]; then
+    message "Apakah Anda Akan Menggunakan file .pem Yang Sudah Pernah Anda Inputkan ? (y/n)"
+    read -p "Your Answer : " pem_in_pem_directory
+
+    if [ "$pem_in_pem_directory" == "y" ]; then
+      # Mendapatkan file .pem yang tersedia di pem_directory
+      pem_files=($(ls -p /home/Cronjob-TACP/RsyncServer(.pem) | grep -v /))
+      num_pem_files=${#pem_files[@]}
+
+      if [ "$num_pem_files" -eq 0 ]; then
+        error_message "Tidak ada file .pem yang tersedia di pem_directory."
+        return
+      fi
+
+      # Menampilkan list file .pem yang tersedia di pem_directory
+      message "Pilih file .pem yang akan digunakan:"
+      for ((i=0; i<$num_pem_files; i++)); do
+        echo "$(($i+1)). ${pem_files[$i]}"
+      done
+
+      selected_pem_index=0
+      while [[ ! "$selected_pem_index" =~ ^[0-9]+$ || "$selected_pem_index" -lt 1 || "$selected_pem_index" -gt $num_pem_files ]]; do
+        read -p "Masukkan nomor pilihan (1-$num_pem_files):" selected_pem_index
+      done
+
+      selected_pem_file="${pem_files[$(($selected_pem_index-1))]}"
+      pem_path="/home/Cronjob-TACP/RsyncServer(.pem)/$selected_pem_file"
+    else
+      message "Masukkan path file .pem:"
+      read -p "Your Answer : " pem_path
+
+      if [ ! -f "$pem_path" ]; then
+        error_message "File .pem tidak ditemukan."
+        return
+      fi
+
+      # Copy file .pem ke pem_directory
+      pem_filename=$(basename "$pem_path")
+      pem_directory="/home/Cronjob-TACP/RsyncServer(.pem)"
+      sudo mkdir -p "$pem_directory"
+      sudo chmod 755 "$pem_directory"
+      sudo cp "$pem_path" "$pem_directory/$pem_filename"
+      pem_path="$pem_directory/$pem_filename"
+
+      # Memberikan perizinan chmod pada file .pem
+      sudo chmod 400 "$pem_path"
+    fi
+
+    # Perintah rsync
+    rsync_command="rsync -$options --progress -e 'ssh -p $port -i $pem_path' $source_path $username@$hostname:$destination_path"
+
+    # Menjalankan perintah rsync
+    $rsync_command
+
+    # Memeriksa status keluaran rsync
+    if [ $? -eq 0 ]; then
+      success_message "Rsync berhasil dilakukan."
+
+      rsync_script_path="/home/Cronjob-TACP/RsyncServer(.pem)/HasilRsync_$(date +'%Y%m%d%H%M%S').sh"
+
+      # Membuat file script HasilRsync.sh
+      echo "#!/bin/bash" | sudo tee "$rsync_script_path" > /dev/null
+      echo "$rsync_command" | sudo tee -a "$rsync_script_path" > /dev/null
+
+      success_message "File HasilRsync.sh telah dibuat di $rsync_script_path"
+
+      # Menambahkan cronjob untuk menjalankan script HasilRsync.sh
+      message "Apakah Anda ingin menambahkan cronjob untuk menjalankan script HasilRsync.sh? (y/n)"
+      read -p "Your Answer : " add_cronjob
+
+      if [ "$add_cronjob" == "y" ]; then
+        message "Masukkan waktu cronjob (dalam hitungan menit):"
+        read -p "Your Answer : " cronjob_minutes
+
+        # Menambahkan cronjob
+        (crontab -l ; echo "*/$cronjob_minutes * * * * bash $rsync_script_path") | crontab -
+        success_message "Cronjob telah ditambahkan."
+      else
+        success_message "Cronjob tidak ditambahkan."
+      fi
+    else
+      error_message "Rsync gagal dilakukan."
+    fi
+  else
+    warning_message "Private key RSA tidak digunakan."
+    pem_path=""
+    rsync_command="rsync -$options --progress -e 'ssh -p $port' $source_path $username@$hostname:$destination_path"
+    $rsync_command
+
+    # Memeriksa status keluaran rsync
+    if [ $? -eq 0 ]; then
+      success_message "Rsync berhasil dilakukan."
+    else
+      error_message "Rsync gagal dilakukan."
+    fi
+  fi
 }
 
 
-# Menampilkan menu utama
-while true; do
-  echo "+-----------------------------------------+"
-  echo "|                 Menu                    |"
-  echo "+-----------------------------------------+"
-  echo "|  1. Rsync Lokal                         |"
-  echo "|  2. Rsync ke Server Remote              |"
-  echo "|  3. Keluar                              |"
-  echo "+-----------------------------------------+"
-  echo "Your Answer : "
-  read -p "Pilih menu (1-3):" choice
 
-  case $choice in
-    1)
-      perform_local_rsync
-      ;;
-    2)
-      perform_remote_rsync
-      ;;
-    3)
-      break 2
-      ;;
-    *)
-      error_message "Pilihan tidak valid."
-      ;;
-  esac
-done
+# Memulai program rsync-TACP
+message "Selamat datang di rsync-TACP!"
+message "Silakan pilih opsi rsync yang ingin Anda gunakan:"
+echo "+-----+-------------------------------+"
+echo "| No  |        Jenis Rsync           |"
+echo "+-----+-------------------------------+"
+echo "|  1  | Rsync ke server lokal        |"
+echo "|  2  | Rsync ke server remote       |"
+echo "|  3  | Rsync ke server remote (pem) |"
+echo "+-----+-------------------------------+"
+read -p "Masukkan nomor pilihan Anda:" rsync_type
+
+case $rsync_type in
+  1)
+    perform_local_rsync
+    ;;
+  2)
+    perform_remote_rsync_with_password
+    ;;
+  3)
+    perform_remote_rsync_with_pem
+    ;;
+  *)
+    error_message "Opsi tidak valid. Silakan pilih nomor yang valid."
+    ;;
+esac
+
+message "Terima kasih telah menggunakan rsync-TACP!"
+
+  #ini adalah perintah yang di butuhkan server target 
+  #sudo chown -R ec2-user:ec2-user /home/ec2-user/app-log/
+  #sudo chmod -R 755 /home/ec2-user/app-log/
